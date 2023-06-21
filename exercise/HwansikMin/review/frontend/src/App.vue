548d12113e9846fdb483bd36d3d19bdb497ea2d3
hwansik <hwansik08@gmail.com>
2023-06-21 18:36:08 +0900

    feat: [민환식] vue review 230621 [REVIEWx]

--- exercise/HwansikMin/review/frontend/src/App.vue
+++ exercise/HwansikMin/review/frontend/src/App.vue
@@ -15,7 +15,8 @@
         이 작업을 자동화하려면 반드시 router/index.js에
         /htmlTest를 누가 보여줄 것인지 컴포넌트 등록을 해줘야 합니다.
       -->
-      <router-link to="/htmlTest">HtmlTest</router-link>
+      <router-link to="/htmlTest">HtmlTest</router-link> |
+      <router-link to="/cssTest">cssTest</router-link> |
     </nav>
     <router-view/>
   </div>
--- exercise/HwansikMin/review/frontend/src/router/index.js
+++ exercise/HwansikMin/review/frontend/src/router/index.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router'
 // @는 현재 src 폴더를 의미함.
 import HomeView from '@/views/HomeView.vue'
 import HtmlTest from '@/views/html/HtmlTest.vue'
-
+import CssTest from '@/views/css/CssTest.vue'
 /*
 import 'vue파일명' from '@(src)/파일경로' 
 위 형식으로 router설정을 해줘야한다.
@@ -29,6 +29,11 @@ const routes = [
     path: '/htmlTest',
     name: 'HtmlTest',
     component: HtmlTest
+  },
+  {
+    path: '/cssTest',
+    name: 'CssTest',
+    component: CssTest
   }
 ]
 
